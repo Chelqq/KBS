55c001f84ef608fd8f9fa4dfef3fb03bd97e32bc
Chel <cdeszz8@gmail.com>
2023-08-28 10:14:30 -0600

    java started, workin on it

---
+++ JAVA/practica1_holamundo/src/main/java/com/mycompany/practica1_holamundo/Practica1_holamundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1_holamundo;
+
+/**
+ *
+ * @author compa
+ */
+public class Practica1_holamundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
