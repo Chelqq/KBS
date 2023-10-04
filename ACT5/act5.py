e50046a6dc5450229f3083b3c1ea08f1ae8d3f02
Chel <cdeszz8@gmail.com>
2023-10-04 00:06:27 -0600

    dunno

--- ACT5/act5.py
+++ ACT5/act5.py
@@ -26,14 +26,34 @@ new_kb([
     "supervisor_de('Kamala harris', 'Arvind krisman')",
     "supervisor_de('Jared Kushner', 'Steve wozniak')",
     "supervisor_de('Jared Kushner', 'Arvind krisman')",
-    "supervisor_de('Kamala harris', 'Steve wozniak')",
+    "supervisor_de('Kamala harris', 'Steve wozniak')"])
     
     # Reglas
-    "es_director(X) :- director_de(X, _)",
-    "es_gerente(X) :- gerente_de(X, _)",
-    "es_supervisor(X) :- supervisor_de(X, _)",
-    "es_ingeniero(X) :- hombre(X), (supervisor_de(_, X) ; gerente_de(_, X))",
-    "ceo(X) :- ceo_de(X, _)",
-])
+
+    # Define the CEO rule
+pl.rule(ceo_de(X, Y), [X == "Elon Musk", Y == "Tim Cook"])
+
+    # Define the Director General rule
+pl.rule(director_general(X, Y), [X == "Tim Cook", Y == "Barak Obama"])
+pl.rule(director_general(X, Y), [X == "Tim Cook", Y == "Joe Bezos"])
+pl.rule(director_general(X, Y), [X == "Tim Cook", Y == "Donald Trump"])
+pl.rule(director_general(X, Y), [X == "Tim Cook", Y == "Joe Biden"])
+pl.rule(director_general(X, Y), [X == "Tim Cook", Y == "Kamala Harris"])
+
+    # Define the Manager rule
+pl.rule(manager(X, Y), [X == "Jared Kushner", Y == "Mark Zuckerberg"])
+pl.rule(manager(X, Y), [X == "Barak Obama", Y == "Mark Zuckerberg"])
+pl.rule(manager(X, Y), [X == "Kamala Harris", Y == "Mark Zuckerberg"])
+pl.rule(manager(X, Y), [X == "Mark Zuckerberg", Y == "Jared Kushner"])
+pl.rule(manager(X, Y), [X == "Joe Biden", Y == "Jared Kushner"])
+pl.rule(manager(X, Y), [X == "Joe Bezos", Y == "Jared Kushner"])
+
+    # Define the Supervisor rule
+pl.rule(supervisor(X, Y), [X == "Mark Zuckerberg", Y == "Steve Wozniak"])
+pl.rule(supervisor(X, Y), [X == "Mark Zuckerberg", Y == "Arvind Krisman"])
+pl.rule(supervisor(X, Y), [X == "Kamala Harris", Y == "Arvind Krisman"])
+pl.rule(supervisor(X, Y), [X == "Jared Kushner", Y == "Steve Wozniak"])
+pl.rule(supervisor(X, Y), [X == "Jared Kushner", Y == "Arvind Krisman"])
+pl.rule(supervisor(X, Y), [X == "Kamala Harris", Y == "Steve Wozniak"])
 
 print(new_kb.query(pl.Expr("supervisor_de('', 'Mark zuckerberg')")))
